25af924c87379c4b748bbf67cbd231dcac43453c
Mingkun Ma <mmk.ee.911@gmail.com>
2022-07-06 21:02:23 -0700

    feat: Add basic layout of content

---
+++ layouts/Content/index.tsx
@@ -0,0 +1,49 @@
+import { FunctionComponent } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+interface ParkingDate {
+  date: string;
+  year: string;
+  time: string;
+}
+
+interface ContentProps {
+  location: {
+    parkingLot: string;
+    address: string;
+    spot: string;
+  };
+  date: {
+    startDate: ParkingDate;
+    endDate: ParkingDate;
+  };
+  instructions: string;
+  purchaseAdditionalMonth: string;
+  amount: string;
+  user: {
+    avatar: string;
+    name: string;
+    owner?: string;
+    message: string;
+  };
+}
+
+const Content: FunctionComponent<ContentProps> = ({
+  location,
+  date,
+  instructions,
+  purchaseAdditionalMonth,
+  amount,
+  user,
+}) => {
+  return <View style={styles.container}></View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+  },
+});
+
+export default Content;
